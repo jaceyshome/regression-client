0d2e18519534566fef6b1d4070f18b8002a4ad85
Jake Wang <jaceyshome@gmail.com>
2018-10-21 16:26:44 +1100

    add yarn.lock and remove docker-compose.yml, mv it to regression-system directory, finalise dependencies, add node user for the docker images

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
     env: 'DEV', //DEV or PROD
+    //regression-test-server root path
     apiRootPath: 'http://localhost:7071/',
+    //regression-test-puppeteer server root path
     outputPath: 'http://localhost:7080/',
 };
