f6a7de254956721d8cff6b4539516a1cf8a3ead2
Jake Wang <jaceyshome@gmail.com>
2018-10-23 14:32:02 +1100

    correct api root path for the server site

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: 'DEV', //DEV or PROD
     //regression-test-server root path
-    apiRootPath: 'http://localhost:7071/',
+    apiRootPath: 'http://54.79.0.252:7071/',
     //regression-test-puppeteer server root path
-    outputPath: 'http://localhost:7080/',
+    outputPath: 'http://54.79.0.252:7080/',
 };
